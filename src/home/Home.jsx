74e37bc567fca8b61919764bfd47f7a541c40f64
phantware <phantmoney2011@gmail.com>
2022-10-31 21:23:15 +0100

    links added to the social media page

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -6,12 +6,48 @@ const Home = () => {
   return (
     <div className='homeContainer'>
       <Profile />
+
       <SocialCard title='Twitter Link' />
-      <SocialCard title='Zuri Team' />
-      <SocialCard title='Zuri Book' />
-      <SocialCard title='Python Book' />
-      <SocialCard title='Background Check for Coders' />
-      <SocialCard title='Design Books' />
+      <a
+        href='https://training.zuri.team/'
+        target='_blank'
+        rel='noreferrer'
+        id='btn__zuri'
+      >
+        <SocialCard title='Zuri Team' />
+      </a>
+      <a
+        href='https://books.zuri.team/'
+        target='_blank'
+        rel='noreferrer'
+        id='books'
+      >
+        <SocialCard title='Zuri Book' />
+      </a>
+      <a
+        href='https://books.zuri.team/'
+        target='_blank'
+        rel='noreferrer'
+        id='book__python'
+      >
+        <SocialCard title='Python Book' />
+      </a>
+      <a
+        href='https://background.zuri.team/'
+        target='_blank'
+        rel='noreferrer'
+        id='pitch'
+      >
+        <SocialCard title='Background Check for Coders' />
+      </a>
+      <a
+        href='https://books.zuri.team/design-rules'
+        target='_blank'
+        rel='noreferrer'
+        id='book__design'
+      >
+        <SocialCard title='Design Books' />
+      </a>
       <SocialCard lolo='lolo' />
     </div>
   )
