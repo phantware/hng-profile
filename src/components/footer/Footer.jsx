89a3f9c42cbae2d9139e17f6322d3ed95ce8bad1
phantware <phantmoney2011@gmail.com>
2022-10-31 23:32:17 +0100

    footer component styled

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,9 +5,9 @@ import I4G from '../../assets/I4G.svg'
 const Footer = () => {
   return (
     <div className='footerContainer'>
-      <img src={zuri} alt='zuri' />
+      <img src={zuri} alt='zuri' className='zuri' />
       <h1 className='footerTitle'>HNG Internship 9 Frontend Task</h1>
-      <img src={I4G} alt='Ingressive food' />
+      <img src={I4G} alt='Ingressive food' className='ingressive' />
     </div>
   )
 }
