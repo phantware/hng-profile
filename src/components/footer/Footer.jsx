f9af26fadebc084848ffe7c4b21983890fc987cb
phantware <phantmoney2011@gmail.com>
2022-11-01 00:31:12 +0100

    footer made responsive

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,11 @@
 import './footer.css'
-import zuri from '../../assets/zuri.svg'
+import zuri2 from '../../assets/zuri2.jpg'
 import I4G from '../../assets/I4G.svg'
 
 const Footer = () => {
   return (
     <div className='footerContainer'>
-      <img src={zuri} alt='zuri' className='zuri' />
+      <img src={zuri2} alt='zuri' className='zuri' />
       <h1 className='footerTitle'>HNG Internship 9 Frontend Task</h1>
       <img src={I4G} alt='Ingressive food' className='ingressive' />
     </div>
