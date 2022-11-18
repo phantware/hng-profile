4ba88979509c3340dc338a215cfdd73df33a7646
phantware <phantmoney2011@gmail.com>
2022-11-18 17:33:20 +0100

    routers added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import Home from './pages/home/Home'
 import About from './pages/about/About'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
 function App() {
   return (
     <div>
-      <Home />
-      <About />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
