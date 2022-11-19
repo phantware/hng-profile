f491397c55a30f2618d36abb634adaa10fc08463
phantware <phantmoney2011@gmail.com>
2022-11-19 12:29:23 +0100

    navbar added to App.js

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import About from './pages/about/About'
 import Contact from './pages/contact/Contact'
 import Shop from './pages/shop/Shop'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Navbar from './components/navbar/Navbar'
 
 function App() {
   return (
     <div>
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/about' element={<About />} />
-          <Route path='/about' element={<Contact />} />
-          <Route path='/about' element={<Shop />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/shop' element={<Shop />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,27 @@
+import { Link } from 'react-router-dom'
 import './navbar.css'
 
 const Navbar = () => {
-  return <div>Navbar</div>
+  return (
+    <div className='navbar'>
+      <Link to='/'>Logo</Link>
+
+      <ul className='nav-links'>
+        <li className='nav-items'>
+          <Link to='/'>Home</Link>
+        </li>
+        <li className='nav-items'>
+          <Link to='/about'>About</Link>
+        </li>
+        <li className='nav-items'>
+          <Link to='/shop'>Shop</Link>
+        </li>
+        <li className='nav-items'>
+          <Link to='/contact'>Contact</Link>
+        </li>
+      </ul>
+    </div>
+  )
 }
 
 export default Navbar
